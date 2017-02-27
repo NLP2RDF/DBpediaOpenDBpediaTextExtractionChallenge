4ffa682d0ea3dda5cc5a3a09759ffd779133d8f7
Sandro <sandroacoelho@gmail.com>
2017-02-26 21:29:05 -0300

    Update DBpediaOpenDBpediaTextExtractionChallenge.java

--- src/main/java/org/dbpedia/extraction/DBpediaOpenDBpediaTextExtractionChallenge.java
+++ src/main/java/org/dbpedia/extraction/DBpediaOpenDBpediaTextExtractionChallenge.java
@@ -2,7 +2,7 @@ package org.dbpedia.extraction;
 
 public class DBpediaOpenDBpediaTextExtractionChallenge {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
         System.out.println("Start here!");
 
